Fix Sigmoid, add Softmax

layer.py
@@ -7,16 +7,13 @@ class Layer:
         self.weights = None
         self.bias = None
 
-    def predict(self, features):
+    def forward(self, features):
         return features
-
-    def take_gradient(self, gradients):
-        pass
 
     def update(self, learning_rate):
         pass
 
-    def backward(self, loss):
+    def backward(self, gradients):
         return None
 
 class Perceptron(Layer):
@@ -27,35 +24,37 @@ class Perceptron(Layer):
         self.weights = np.random.randn(num_weights, num_perceptrons) * scale
         self.bias = np.random.randn(1, num_perceptrons)
         #self.weights = np.ones((num_weights, num_perceptrons))
-        self.bias = np.zeros((1, num_perceptrons))
+        #self.bias = np.zeros((1, num_perceptrons))
 
-    def predict(self, features):
+    def forward(self, features):
         if self.weights.shape[0] != features.shape[1]:
             raise ValueError('Unmatched weights and features')
 
         self.catch_features = features.T
         return np.dot(features, self.weights) + self.bias
 
-    def take_gradient(self, gradients):
+    def backward(self, gradients):
         if self.catch_features is None:
             raise ValueError('No catch features')
-        self.gradients = np.dot(self.catch_features, gradients)
-        self.bias_gradient = gradients.sum()
+        self.gradients = np.dot(self.catch_features, gradients) / gradients.shape[0]
+        self.bias_gradient = gradients.mean(axis=0)
         self.catch_features = None
+
+        return np.dot(gradients, self.weights.T)
 
     def update(self, learning_rate):
         if (self.gradients is None) or (self.bias_gradient is None):
             raise ValueError('No gradients')
-        self.weights += learning_rate * self.gradients
-        #self.bias += learning_rate * self.bias_gradient
+        self.weights -= learning_rate * self.gradients
+        self.bias -= learning_rate * self.bias_gradient
 
-        #print('Bias')
+        #print('weight')
+        #print(self.weights)
+        #print('bias')
         #print(self.bias)
         self.gradients = None
         self.bias_gradient = None
-
-    def backward(self, loss):
-        return np.dot(loss, self.weights.T)
+        
 
 class Sigmoid(Layer):
     def __init__(self, A=1, k=1, c=0):
@@ -64,23 +63,32 @@ class Sigmoid(Layer):
         self.k = k
         self.c = c
 
-    def predict(self, features):
-        return self.A / (1 + np.exp(-self.k * (features - self.c)))
+    def forward(self, features):
+        self.predictions = self.A / (1 + np.exp(-self.k * (features - self.c)))
+        return self.predictions
 
     def backward(self, loss):
-        return loss * self.predict(loss) * (1 - self.predict(loss))
+        return loss * self.predictions * (1 - self.predictions)
 
 class Softmax(Layer):
     def __init__(self, T=1):
         super().__init__()
         self.T = T
 
-    def predict(self, features):
-        return np.exp(self.T * features) / np.exp(self.T * features).sum()
+    def forward(self, features):
+        f_max = features.max(axis=1, keepdims=True)
+        f_exp = np.exp(features - f_max)
+        f_sum = f_exp.sum(axis=1, keepdims=True)
+        self.predictions = f_exp / f_sum
+        return self.predictions
+
+    def backward(self, gradients):
+        pred_grad = self.predictions * gradients
+        return pred_grad - self.predictions * pred_grad.sum(axis=1, keepdim=True)
 
 class ReLU(Layer):
     def __init__(self):
         super().__init__()
 
-    def predict(self, features):
+    def forward(self, features):
         return (features > 0) * features

test.py
@@ -1,13 +1,21 @@
 import numpy as np
 
-array = np.array([[3, 2, 3], [4, 7, 7], [7, 8, 9]])
-array = np.array([i == i.max() for i in array])
+class Sigmoid():
+    def __init__(self, A=1, k=1, c=0):
+        super().__init__()
+        self.A = A
+        self.k = k
+        self.c = c
 
-print(array)
-print('-------------------------')
-for arr in array:
-    if arr.sum() > 1:
-        f = next(i for i, a in enumerate(arr) if a)
-        for i in range(len(arr)):
-            arr[i] = False if i > f else arr[i]
-print(array)
+    def forward(self, features):
+        self.predictions = self.A / (1 + np.exp(-self.k * (features - self.c)))
+        return self.predictions
+
+    def backward(self, loss):
+        return loss * self.forward(loss) * (1 - self.forward(loss))
+
+sig = Sigmoid()
+forth = sig.forward(np.array([[1]]))
+print(forth)
+back = sig.backward(forth - 1)
+print(back)